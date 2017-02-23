c0e903c3dab9fea0594d023ab9c049ca408bd9a4
David Lynch <kemayo@gmail.com>
2017-02-23 14:40:55 -0600

    Cover text: outlined

--- cover.py
+++ cover.py
@@ -13,13 +13,12 @@ def make_cover(title, author, width=600, height=800, fontname="Helvetica", fonts
 
     font = ImageFont.truetype(font=fontname, size=fontsize)
     title_size = draw.textsize(title, font=font)
-    draw.text(((width - title_size[0]) / 2, 100), title, textcolor, font=font)
+    draw_text_outlined(draw, ((width - title_size[0]) / 2, 100), title, textcolor, font=font)
+    # draw.text(((width - title_size[0]) / 2, 100), title, textcolor, font=font)
 
     font = ImageFont.truetype(font=fontname, size=fontsize - 2)
     author_size = draw.textsize(author, font=font)
-    draw.text(((width - author_size[0]) / 2, 100 + title_size[1] + 70), author, textcolor, font=font)
-
-    draw = ImageDraw.Draw(img)
+    draw_text_outlined(draw, ((width - author_size[0]) / 2, 100 + title_size[1] + 70), author, textcolor, font=font)
 
     output = BytesIO()
     img.save(output, "PNG")
@@ -27,6 +26,19 @@ def make_cover(title, author, width=600, height=800, fontname="Helvetica", fonts
     # writing left the cursor at the end of the file, so reset it
     output.seek(0)
     return output
+
+
+def draw_text_outlined(draw, xy, text, fill=None, font=None, anchor=None):
+    x, y = xy
+
+    # Outline
+    draw.text((x - 1, y), text=text, fill=(0, 0, 0), font=font, anchor=anchor)
+    draw.text((x + 1, y), text=text, fill=(0, 0, 0), font=font, anchor=anchor)
+    draw.text((x, y - 1), text=text, fill=(0, 0, 0), font=font, anchor=anchor)
+    draw.text((x, y + 1), text=text, fill=(0, 0, 0), font=font, anchor=anchor)
+
+    # Fill
+    draw.text(xy, text=text, fill=fill, font=font, anchor=anchor)
 
 
 if __name__ == '__main__':
