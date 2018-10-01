929284b67dc06d4c0d646a7f42ad15281c2e28ae
David Lynch <kemayo@gmail.com>
2018-10-01 11:18:39 -0500

    New features for arbitrary sites
    * next_selector: find next content page, if not using chapter selector
    * content_title_selector: pull a chapter title from the content
    * content_text_selector: pull specific text from the content element
    
    `content_selector` will now fetch all content elements on the page, each
    as a Chapter, not just the first one that matches.

--- sites/arbitrary.py
+++ sites/arbitrary.py
@@ -29,9 +29,15 @@ class SiteDefinition:
     title = attr.ib()
     author = attr.ib()
     content_selector = attr.ib()
-    # If this is present, it looks for chapters linked from `url`. If not, it assumes `url` points to a chapter.
+    # If present, find something within `content` to use a chapter title; if not found, the link text to it will be used
+    content_title_selector = attr.ib(default=False)
+    # If present, find a specific element in the `content` to be the chapter text
+    content_text_selector = attr.ib(default=False)
+    # If present, it looks for chapters linked from `url`. If not, it assumes `url` points to a chapter.
     chapter_selector = attr.ib(default=False)
-    # If this is present, it's used to filter out content that matches the selector
+    # If present, use to find a link to the next content page (only used if not using chapter_selector)
+    next_selector = attr.ib(default=False)
+    # If present, use to filter out content that matches the selector
     filter_selector = attr.ib(default=False)
 
 
@@ -57,37 +63,66 @@ class Arbitrary(Site):
 
         if definition.chapter_selector:
             soup = self._soup(definition.url)
-            for chapter in soup.select(definition.chapter_selector):
-                chapter_url = urllib.parse.urljoin(definition.url, str(chapter.get('href')))
-                story.add(Chapter(
-                    title=chapter.string,
-                    contents=self._chapter(chapter_url, definition),
-                    # TODO: better date detection
-                    date=datetime.datetime.now()
-                ))
+            base = soup.head.base and soup.head.base.get('href') or False
+            for chapter_link in soup.select(definition.chapter_selector):
+                chapter_url = str(chapter_link.get('href'))
+                if base:
+                    chapter_url = urllib.parse.urljoin(base, chapter_url)
+                chapter_url = urllib.parse.urljoin(definition.url, chapter_url)
+                for chapter in self._chapter(chapter_url, definition, title=chapter_link.string):
+                    story.add(chapter)
         else:
-            story.add(Chapter(
-                title=definition.title,
-                contents=self._chapter(definition.url, definition),
-                # TODO: better date detection
-                date=datetime.datetime.now()
-            ))
+            content_url = definition.url
+            while content_url:
+                for chapter in self._chapter(content_url, definition):
+                    story.add(chapter)
+                if definition.next_selector:
+                    soup = self._soup(content_url)
+                    base = soup.head.base and soup.head.base.get('href') or False
+                    next_link = soup.select(definition.next_selector)
+                    if next_link:
+                        next_link_url = str(next_link[0].get('href'))
+                        if base:
+                            next_link_url = urllib.parse.urljoin(base, next_link_url)
+                        content_url = urllib.parse.urljoin(content_url, next_link_url)
+                    else:
+                        content_url = False
+                else:
+                    content_url = False
 
         return story
 
-    def _chapter(self, url, definition):
-        # TODO: refactor so this can meaningfully handle multiple matches on content_selector.
-        # Probably by changing it so that this returns a Chapter / Section.
+    def _chapter(self, url, definition, title=False):
         logger.info("Extracting chapter @ %s", url)
         soup = self._soup(url)
 
+        chapters = []
+
         if not soup.select(definition.content_selector):
-            return ''
+            return chapters
 
-        content = soup.select(definition.content_selector)[0]
+        for content in soup.select(definition.content_selector):
+            if definition.filter_selector:
+                for filtered in content.select(definition.filter_selector):
+                    filtered.decompose()
 
-        if definition.filter_selector:
-            for filtered in content.select(definition.filter_selector):
-                filtered.decompose()
+            if definition.content_title_selector:
+                title_element = content.select(definition.content_title_selector)
+                if title_element:
+                    title = title[0].string
 
-        return content.prettify()
+            if definition.content_text_selector:
+                # TODO: multiple text elements?
+                content = content.select(definition.content_text_selector)[0]
+
+            # TODO: consider `'\n'.join(map(str, content.contents))`
+            content.name = 'div'
+
+            chapters.append(Chapter(
+                title=title,
+                contents=content,
+                # TODO: better date detection
+                date=datetime.datetime.now(),
+            ))
+
+        return chapters
